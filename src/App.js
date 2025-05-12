584f1ef3bfe980613c1ba7d929c814bf4bfc3624
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-05-12 18:52:35 +0900

    개별메인으로 이동 링크

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-// import FacilityList from "./pages/FacilityList";
-// import FacilityDetail from "./pages/FacilityDetail";
+import Caregiver from "./pages/Caregiver";
+import NursingHome from "./pages/NursingHome";
+import Slivertown from "./pages/Slivertown";
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/facilities" element={<FacilityList />} />
-        <Route path="/facilities/:id" element={<FacilityDetail />} /> */}
+        <Route path="/caregiver" element={<Caregiver />} />
+        <Route path="/nursinghome" element={<NursingHome />} />
+        <Route path="/slivertown" element={<Slivertown />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/NursingHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NursingHome() {
+  return (
+    <div>
+      <h1>요양원 메인 페이지</h1>
+    </div>
+  );
+}
+
+export default NursingHome;
